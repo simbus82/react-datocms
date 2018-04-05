20203f23f86bf9977a93eba2337a6e2f4e907435
Simone Bussoni <simbus82@gmail.com>
2018-04-06 00:23:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import Films from './Films';
-import Film from './Film'
 import Main from './Main'
+import Articoli from './Articoli'
 import './index.css';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
-      <IndexRoute component={Films} />
-      <Route path="films" component={Films} />
-      <Route path="films/:filmId" component={Film} />
+      <IndexRoute component={Articoli} />
+      <Route path="articoli" component={Articoli} />
+      <Route path="articoli/:articoloId" component={Articolo} />
     </Route>
   </Router>
 ), document.getElementById('root'))
