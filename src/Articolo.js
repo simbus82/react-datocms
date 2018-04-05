b59e23b7be1b37799dc019ef93ce798635bb3edd
Simone Bussoni <simbus82@gmail.com>
2018-04-06 00:27:59 +0200

    Update and rename Film.js to Articolo.js

---
+++ src/Articolo.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import datocmsClient from './DatoCmsClient'
+
+class Articolo extends Component {
+  state = {
+    articolo: {}
+  }
+
+  componentWillMount () {
+    datocmsClient.getArticolo(this.props.params.articoloId)
+      .then((articolo) => this.setState({ articolo }))
+  }
+
+  render () {
+    const { titolo, contenuto_introduttivo, contenuto_principale } = this.state.articolo
+
+    return (
+      <div className='Articolo'>
+        <h1>{title}</h1>
+        <h2>Questo è il contenuto introduttivo</h2>
+        <p>{contenuto_introduttivo}</p>
+        <br/>
+        <h2>Questo è il contenuto principale</h2>
+        <p>{contenuto_introduttivo}</p>
+        <br/>
+      </div>
+    )
+  }
+}
+
+export default Articolo
