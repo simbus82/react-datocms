f767781b764a8357295afe900f86f85074d082a7
Simone Bussoni <simbus82@gmail.com>
2018-04-06 00:33:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import Main from './Main'
 import Articoli from './Articoli'
+import Articolo from './Articolo'
 import './index.css';
 
 ReactDOM.render((
